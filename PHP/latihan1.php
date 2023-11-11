5e28d3c5242519ee50db162b5bb39555122476f1
unknown <ayuresa2711@gmail.com>
2023-11-11 23:24:49 +0700

    sukses

---
+++ PHP/latihan1.php
@@ -0,0 +1,21 @@
+<?php
+
+$a ="hello";
+$hello = "Hello World!";
+
+//menampilkan isi variable $a
+//hello
+echo $a . "</br>";
+
+//menampilkan isi variable $a
+//Hello World!
+echo $hello . "</br>";
+
+//menampilkan isi variable dengan nama yang sama seperti isi variable $a
+//isi variable $a = hello. jadi, nanti akan menampilkan isi dari variable
+$hello
+// Hello World
+echo $$a . "</br>";
+
+?>
+
---
+++ PHP/variable.php
@@ -0,0 +1,14 @@
+<?php
+
+$nrp = "173040888";
+$nama = "loren ipsum;";
+$umur = 20;
+$nilai = 75.75;
+
+echo "Nrp   :  ". $nrp . "</br>";
+echo "Nama  :  $nama</br>";
+print "Umur : ".$umur; print "</br>";
+//menampilkan bilangan real dengan format 3 digit belakang koma
+printf ("Nilai : %.3f</br>", $nilai);
+
+?>
